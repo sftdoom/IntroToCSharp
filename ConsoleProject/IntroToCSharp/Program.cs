610ca8d13234644c73df0d7af7a1dd29e0d937da
Cameron Danitschek <cameron.danitschek@gmail.com>
2022-08-25 09:44:42 -0700

    Add initial project files

---
+++ ConsoleProject/IntroToCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AIE
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Cameron Danitschek");
+
+            //Variable TYPE NAME (value)
+            int age = 18;
+
+            Console.WriteLine(age);
+
+            Console.WriteLine("What is your favorite peice of candy?");
+            string favCandyInput = Console.ReadLine();
+            Console.WriteLine("Your favorite candy is " + favCandyInput);
+
+            if(favCandyInput== "Crunch")
+            {
+                Console.WriteLine("Crunch? Not bad not bad.");
+            }
+
+            string numberInput = Console.ReadLine();
+            int userNumber = 0;
+            int.TryParse(numberInput, out userNumber);
+            Console.WriteLine("You wrote the number " + userNumber);
+            Console.WriteLine("If squared, it would be " + userNumber * userNumber);
+
+        }
+
+        
+    }
+}
+
